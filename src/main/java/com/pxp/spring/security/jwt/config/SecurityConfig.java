2d8303f6688e9821caf4f55059537196b56f05ff
rajesh1420621 <panigrahirajesh578@gmail.com>
2021-01-11 22:54:38 +0530

    Spring Security JWT.

--- src/main/java/com/pxp/spring/security/jwt/config/SecurityConfig.java
+++ src/main/java/com/pxp/spring/security/jwt/config/SecurityConfig.java
@@ -1,18 +1,22 @@
 package com.pxp.spring.security.jwt.config;
 
+import com.pxp.spring.security.jwt.filter.JwtFilter;
 import com.pxp.spring.security.jwt.services.PXPUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +24,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private PXPUserDetailsService pxpUserDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -36,10 +43,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.cors();
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.GET,"/info")
+                .antMatchers(HttpMethod.POST, "/authenticate");
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public AuthenticationManager getAuthenticationManager() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
--- src/main/java/com/pxp/spring/security/jwt/controller/SecurityController.java
+++ src/main/java/com/pxp/spring/security/jwt/controller/SecurityController.java
@@ -1,11 +1,29 @@
 package com.pxp.spring.security.jwt.controller;
 
+import com.pxp.spring.security.jwt.models.AuthRequestModel;
+import com.pxp.spring.security.jwt.services.JwtService;
+import com.pxp.spring.security.jwt.services.PXPUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SecurityController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private PXPUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtService jwtService;
 
     @RequestMapping(value = "info", method = RequestMethod.GET)
     public String info(){
@@ -15,5 +33,18 @@ public class SecurityController {
     @RequestMapping(value = "hello",method = RequestMethod.GET)
     public String hello(){
         return "Hello PXP!";
+    }
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public String getJwtAuthToken(@RequestBody AuthRequestModel authRequestModel) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequestModel.getUserName(), authRequestModel.getPassword()));
+        }catch (BadCredentialsException e){
+            throw new Exception("Incorrect Username or password...", e);
+        }
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(authRequestModel.getUserName());
+        final String jwt = jwtService.getJWT(userDetails);
+        return jwt;
     }
 }
